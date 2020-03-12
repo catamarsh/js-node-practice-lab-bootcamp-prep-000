b771de710a3828b82bedc283941f99922be5bc9b
Cat Marsh <c@catamarsh.com>
2020-03-12 23:32:20 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Cat"
-var height = 63+1
+var height = 63
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height)
+console.log(height+1)
